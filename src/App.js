cb2f9abed268408e41e4bff2f8fdf34d069ffea7
Ming Liu <mliu@macpro.home>
2019-05-06 15:03:58 -0400

    mvp for first project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { Route } from 'react-router-dom';
+
 
 const App = () => (
   <div>
+    <Route exact path="/" component={Home}/>
+    <Route path="/contact" component={Contact}/>
+    <Route path="/about" component={About}/>
     <Navigation />
   </div>
 );
